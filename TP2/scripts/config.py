c088e148bbf84a5646facc4ad8519d7ebcfa6f47
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2021-05-22 19:22:42 -0300

    tp2: Poner sólo las universidades analizadas en la config

--- TP2/scripts/config.py
+++ TP2/scripts/config.py
@@ -6,20 +6,20 @@ logging.basicConfig(level=logging.INFO)
 
 NUMTRACES = 30
 UNIVERSITIES = [
-  'uba.ar',            # América, Argentina (misma ciudad)
+  #'uba.ar',            # América, Argentina (misma ciudad)
   'unc.edu.ar',        # América, Argentina (distinta ciudad)
-  'usp.br',            # América, Brasil
-  'mit.edu',           # América, USA (costa este)
-  'berkeley.edu',      # América, USA (costa oeste)
-  'epfl.ch',           # Europa, Suiza (europa continental del oeste)
+  #'usp.br',            # América, Brasil
+  #'mit.edu',           # América, USA (costa este)
+  #'berkeley.edu',      # América, USA (costa oeste)
+  #'epfl.ch',           # Europa, Suiza (europa continental del oeste)
   'itmo.ru',           # Europa, Rusia (europa continental del este)
-  'nottingham.ac.uk',  # Europa, Reino Unido (islas europeas)
+  #'nottingham.ac.uk',  # Europa, Reino Unido (islas europeas)
   'fs.ru.is',          # Europa, Islandia (islas europeas)
   'unisa.ac.za',       # África, Sudáfrica (sur de áfrica)
   'alexu.edu.eg',      # África, Egipto (noreste de áfrica)
-  'www.u-tokyo.ac.jp', # Asia, Japón (islas asiáticas)
-  'sydney.edu.au',     # Oceanía, Australia
-  'hawaii.edu',        # Oceanía, USA, Hawái (islas de la polinesia)
+  #'www.u-tokyo.ac.jp', # Asia, Japón (islas asiáticas)
+  #'sydney.edu.au',     # Oceanía, Australia
+  #'hawaii.edu',        # Oceanía, USA, Hawái (islas de la polinesia)
 ]
 
 PLOTTED_UNIVERSITIES = [
