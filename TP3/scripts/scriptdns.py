5d0370fffe78761b81044700fda05c106aa0d0cd
fsabatini <sabatinifedericoagustin@gmail.com>
2021-06-11 22:33:12 -0300

    Actualizado el script a una versión casi final

--- TP3/scripts/scriptdns.py
+++ TP3/scripts/scriptdns.py
@@ -1,39 +1,68 @@
 from scapy.layers.inet import IP, UDP, RandShort
-from scapy.layers.dns import DNS, DNSQR
+from scapy.layers.dns import DNS, DNSQR, dnstypes
 from scapy.sendrecv import sr
-from config import hosts, verbose
+from config import hosts, initial_nameserver, verbose
 
-def procesar_respuesta(dns_answer):
-    print("Additional Records")
-    for record in (dns_answer[DNS].ar or ()):
-        print(repr(record))
-    print("Name Servers")
-    for nameserver in (dns_answer[DNS].ns or ()):
-        print(repr(nameserver))
-    print("Answer")
-    for answer in (dns_answer[DNS].an or ()):
-        print(repr(answer))
+dnstype_for = { value: key for key, value in dnstypes.items() }
 
-#ip = IP(dst="199.9.14.201")
-ip = IP(dst="1.1.1.1")
-udp = UDP(sport=RandShort(), dport=53)
-cola = [ip / udp / DNS(rd=1, qd=DNSQR(qname=host, qtype='MX')) for host in hosts]
+def procesar_respuesta(query, answer, packet_queue):
+    consulta = repr(query[DNS].qd)
+    host = query[DNS].qd.qname.decode('ascii')
+    dns_answer = answer[DNS]
+    soa = [record for record in (dns_answer.ns or ()) if record.type == dnstype_for['SOA']]
+
+    if len(soa) > 1:
+        print(f"!![{host}] Hay más de un SOA, esto viola el estándar, no proseguimos con el análisis")
+        return
+
+    nameservers = [record.rdata[:-1].decode('ascii') for record in (dns_answer.ns or ()) if record.type == dnstype_for['NS']]
+    nameservers.extend(record.mname[:-1].decode('ascii') for record in soa)
+    print(f"[{host}] Encontramos los nameservers: {nameservers}")
+
+    if dns_answer.an is None and not nameservers:
+        print(f"!![{host}] No hubo respuesta MX ni nameservers, fallo en {consulta}")
+    elif dns_answer.an is None:
+        if soa:
+            nuevo_host = soa[0].rrname[:-1].decode('ascii')
+            if host == nuevo_host:
+                print('!!Hay un registro SOA equivalente a la consulta realizada, no se encontró registro MX')
+                return
+
+            print(f'##[{host}] Hay un registro de SOA, cambiando la consulta de {host} a {nuevo_host}')
+            host = nuevo_host
+        packet_queue.extend(mandar_consulta_a(server, host) for server in nameservers)
+        print(f"##[{host}] Reintentando con los nameservers en la respuesta: {nameservers}")
+    else:
+        for answer in (dns_answer.an or ()):
+            print(f'[{host}] {repr(answer)}')
+
+def mandar_consulta_a(server, host):
+    ip = IP(dst=server)
+    udp = UDP(sport=RandShort(), dport=53)
+    return ip / udp / DNS(rd=1, qd=DNSQR(qname=host, qtype='MX'))
 
 def main():
+    cola = [mandar_consulta_a(initial_nameserver, host) for host in hosts]
+    iteracion = 0
     while cola:
+        print(f"=== Mandando los paquetes de la iteración {iteracion} ===")
         answered, unanswered = sr(cola, verbose=verbose, timeout=10)
         cola.clear()
 
         for query in unanswered:
             destino = query[IP].dst
             consulta = repr(query[DNS].qd)
-            print(f'!!Consulta DNS por {consulta} hacia {destino} no fué respondida')
+            host = query[DNS].qd.qname.decode('ascii')
+            print(f'!![{host}] Consulta DNS hacia {destino} no fué respondida')
 
-        for query_answer in answered:
-            destino = query_answer[0][IP].dst
-            consulta = repr(query_answer[0][DNS].qd)
-            print(f'Consulta DNS por {consulta} hacia {destino} fué respondida')
-            procesar_respuesta(query_answer[1])
+        for query, answer in answered:
+            destino = query[IP].dst
+            consulta = repr(query[DNS].qd)
+            host = query[DNS].qd.qname.decode('ascii')
+            print(f'[{host}] Consulta DNS hacia {destino} fué respondida')
+            procesar_respuesta(query, answer, cola)
+        
+        iteracion += 1
 
 if __name__ == '__main__':
     main()
